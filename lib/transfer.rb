a1bfe6702bf6a964ff34c3c67ecafb39691b3628
howard suarez <howardesuarez@gmail.com>
2020-09-06 02:38:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "rejected"
+      @status = "Transaction rejected. Please check your account balance."
       return "Transaction rejected. Please check your account balance."
     end
     
