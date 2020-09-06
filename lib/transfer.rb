6c29ebd8c8f6ab717d9a0ee6eba386f9dda963fd
howard suarez <howardesuarez@gmail.com>
2020-09-06 02:37:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,11 +17,12 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      @status = ""
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
+    
   end
 
   def reverse_transfer
